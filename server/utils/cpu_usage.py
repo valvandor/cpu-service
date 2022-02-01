7525b70bbda7767391ce1eea05467ab8cc12fed1
valvandor <sincov.vitalii@gmail.com>
2022-02-01 19:27:47 +0300

    feat(background): Change the method of outputting data on cpu usage
    use database as destination for this method

--- server/utils/cpu_usage.py
+++ server/utils/cpu_usage.py
@@ -1,12 +1,19 @@
 import psutil
+from sqlalchemy.orm import Session
 
 from config import CPU_SAMPLE_TIME
+from database import engine
+from models import ContentsCpuUsage
 
 
 def get_cpu_percent():
     return psutil.cpu_percent(interval=CPU_SAMPLE_TIME, percpu=False)
 
 
-def print_cpu_percent():
-    usage_cpu = get_cpu_percent()
-    print(f'Current usage_cpu {usage_cpu}%')
+def write_in_db_cpu_data():
+    cpu_usage_data = ContentsCpuUsage(percent_usage=get_cpu_percent())
+
+    session = Session(engine)
+    session.add(cpu_usage_data)
+    session.commit()
+    session.close()
--- server/utils/sheduler.py
+++ server/utils/sheduler.py
@@ -3,12 +3,12 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from config import JOB_CPU_INTERVAL_TIME
-from utils.cpu_usage import print_cpu_percent
+from utils.cpu_usage import write_in_db_cpu_data
 
 
 def start_cpu_usage_background(time_zone):
     scheduler = BackgroundScheduler(timezone=time_zone)
-    scheduler.add_job(print_cpu_percent, 'interval', seconds=JOB_CPU_INTERVAL_TIME)
+    scheduler.add_job(write_in_db_cpu_data, 'interval', seconds=JOB_CPU_INTERVAL_TIME)
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
     return scheduler
