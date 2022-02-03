778695cc2aa1f5299c1182df229c5e77609a82f1
valvandor <sincov.vitalii@gmail.com>
2022-02-03 18:19:02 +0300

    feat(client): make two test charts

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,33 @@
-const heading = document.createElement('h1');
-heading.textContent = "js is working";
+import { Chart, LineElement, PointElement, LineController, CategoryScale, LinearScale} from 'chart.js';
 
-const root = document.querySelector('#root')
-root.append(heading)
+Chart.register(
+  LineElement,
+  PointElement,
+  LineController,
+  CategoryScale,
+  LinearScale,
+);
+
+const data = {
+  labels: [1,2,3,4,5,6,7],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 2, 192)',
+    tension: 0.1
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data
+};
+
+const ctx1 = document.getElementById('firstChart');
+const ctx2 = document.getElementById('secondChart');
+
+
+const myChart1 = new Chart(ctx1, config);
+const myChart2 = new Chart(ctx2, config);
+
