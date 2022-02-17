3ca1978140baee1a709773d509905d2b4b8d20f8
valvandor <sincov.vitalii@gmail.com>
2022-02-17 18:26:20 +0300

    fix(client): Use client's timezone instead of UTC to display charts
    also decompose exported function  getDateAndTimes

--- client/src/services/chart/helpers/getDateAndTimes.js
+++ client/src/services/chart/helpers/getDateAndTimes.js
@@ -1,34 +1,65 @@
 'use strict'
+import moment from 'moment';
 
-async function getDateAndTimes(array) {
-  const dateArray = [];
-  const timesArray = [];
-  let filteredDatesArray = [];
+moment.locale('ru');
+
+function getDataObjForCurrentTZ(dateTimeString, timeZoneOffsetInHours) {
+  try {
+    return moment(dateTimeString).subtract(timeZoneOffsetInHours, 'hours');
+  } catch(err){
+    console.log(err);
+  }
+}
+
+function getCurrentDate(dateArray) {
+  const filteredDatesArray = [];
 
   try {
-    array.forEach(element => {
-      const [date, time] = element.split('T')
-      dateArray.push(date);
-      timesArray.push(time);
-    });
-
-    dateArray.forEach(element => {
-      if (!filteredDatesArray.includes(element)) {
-        filteredDatesArray.push(element);
+    dateArray.forEach(date => {
+      if (!filteredDatesArray.includes(date)) {
+        filteredDatesArray.push(date);
       }
     });
 
     let currentDate
     if (filteredDatesArray.length === 1){
       currentDate = filteredDatesArray[0];
-    } else { // filteredDatesArray.length === 2
+    } else { // should be filteredDatesArray.length === 2
+      // include both values at the turn of the day
       currentDate = `${filteredDatesArray[0]} — ${filteredDatesArray[1]}`;
     }
+    return currentDate;
+  } catch(err){
+    console.log(err);
+  }
+}
+
+async function getCurrentTimeZoneOffsetInHours(dateTime) {
+  try {
+    return dateTime.getTimezoneOffset() / 60;
+  } catch(err){
+    console.log(err);
+  }
+}
+
+async function getDateAndTimes(array) {
+  try {
+    const dateArray = [];
+    const timesArray = [];
+
+    const offset = await getCurrentTimeZoneOffsetInHours(new Date(array[0]))
+
+    array.forEach(date => { 
+      const data = getDataObjForCurrentTZ(date, offset);
+      timesArray.push(data.format('LTS'));
+      dateArray.push(data.format('ll'));
+    });
+
+    const currentDate = getCurrentDate(dateArray)
 
     return [currentDate, timesArray];
-    
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 };
 
